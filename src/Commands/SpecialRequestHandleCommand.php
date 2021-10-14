da44ed15697387791726ffcecbce099d52d19c7b
XAKEPEHOK <icq-475204@ya.ru>
2021-10-14 14:56:16 +0300

    Fix classname

--- src/Commands/SpecialRequestHandleCommand.php
+++ src/Commands/SpecialRequestHandleCommand.php
@@ -13,7 +13,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class SpecialRequestDispatcherCommand extends QueueHandleCommand
+class SpecialRequestHandleCommand extends QueueHandleCommand
 {
 
     public function __construct()
