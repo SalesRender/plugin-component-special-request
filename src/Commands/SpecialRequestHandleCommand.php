6ee28ff2a7d651d1ca8af094fc247b8755c379f9
XAKEPEHOK <icq-475204@ya.ru>
2021-10-21 20:55:56 +0300

    SpecialRequestTask & SpecialRequestHandleCommand refactoring

--- src/Commands/SpecialRequestHandleCommand.php
+++ src/Commands/SpecialRequestHandleCommand.php
@@ -7,11 +7,14 @@
 
 namespace Leadvertex\Plugin\Components\SpecialRequestDispatcher\Commands;
 
+use GuzzleHttp\Exception\BadResponseException;
+use Leadvertex\Plugin\Components\Guzzle\Guzzle;
 use Leadvertex\Plugin\Components\Queue\Commands\QueueHandleCommand;
 use Leadvertex\Plugin\Components\SpecialRequestDispatcher\Models\SpecialRequestTask;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Throwable;
 
 class SpecialRequestHandleCommand extends QueueHandleCommand
 {
@@ -23,20 +26,86 @@ class SpecialRequestHandleCommand extends QueueHandleCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        /** @var SpecialRequestTask $request */
-        $request = SpecialRequestTask::findById($input->getArgument('id'));
+        /** @var SpecialRequestTask $task */
+        $task = SpecialRequestTask::findById($input->getArgument('id'));
 
-        if (is_null($request)) {
+        if (is_null($task)) {
+            $output->writeln("<error>Request with passed id was not found</error>");
             return Command::INVALID;
         }
 
-        if ($request->send()) {
-            $output->writeln("<fg=green>[{$request->getRequest()->getMethod()}}]</> {$request->getRequest()->getUri()}.");
-            return Command::SUCCESS;
+        $request = $task->getRequest();
+
+        if ($request->isExpired()) {
+            $task->delete();
+            $output->writeln("<comment>Request expired and was deleted</comment>");
+            return Command::INVALID;
         }
 
-        $output->writeln("<fg=red>[{$request->getRequest()->getMethod()}}]</> {$request->getRequest()->getUri()}.");
+        try {
+            $response = Guzzle::getInstance()->request(
+                $request->getMethod(),
+                $request->getUri(),
+                [
+                    'json' => [
+                        'request' => $request->getBody(),
+                        '__task' => [
+                            'createdAt' => $task,
+                            'attempt' => [
+                                'number' => $task->getAttempt()->getNumber(),
+                                'limit' => $task->getAttempt()->getLimit(),
+                                'interval' => $task->getAttempt()->getInterval(),
+                            ],
+                        ]
+                    ],
+                    'timeout' => $task->getHttpTimeout(),
+                ]
+            );
+
+            if ($response->getStatusCode() === $request->getSuccessCode()) {
+                $task->delete();
+                $output->writeln("<info>Success!</info>");
+                return Command::SUCCESS;
+            }
+
+            if (in_array($response->getStatusCode(), $request->getStopCodes())) {
+                $task->delete();
+                $output->writeln("<comment>Received stop-code: {$response->getStatusCode()}</comment>");
+                return Command::INVALID;
+            }
+
+            $task->getAttempt()->attempt($response->getStatusCode());
+
+        } catch (BadResponseException $exception) {
+
+            if (in_array($exception->getResponse()->getStatusCode(), $request->getStopCodes())) {
+                $task->delete();
+                $output->writeln("<comment>Received stop-code: {$exception->getResponse()->getStatusCode()}</comment>");
+                return Command::INVALID;
+            }
+
+            $task->getAttempt()->attempt($exception->getResponse()->getStatusCode());
+            $this->outputThrowable($exception, $output);
+
+        } catch (Throwable $throwable) {
+            $task->getAttempt()->attempt($throwable->getMessage());
+            $this->outputThrowable($throwable, $output);
+        }
+
+        if ($task->getAttempt()->isSpent()) {
+            $task->delete();
+        } else {
+            $task->save();
+        }
+
         return Command::FAILURE;
+    }
+
+
+    private function outputThrowable(Throwable $throwable, OutputInterface $output): void
+    {
+        $output->writeln("<error>{$throwable->getMessage()}</error>");
+        $output->writeln("<error>{$throwable->getTraceAsString()}</error>");
     }
 
 }
--- src/Models/SpecialRequestTask.php
+++ src/Models/SpecialRequestTask.php
@@ -7,10 +7,7 @@
 
 namespace Leadvertex\Plugin\Components\SpecialRequestDispatcher\Models;
 
-use Exception;
-use GuzzleHttp\Exception\BadResponseException;
 use Leadvertex\Plugin\Components\Db\ModelInterface;
-use Leadvertex\Plugin\Components\Guzzle\Guzzle;
 use Leadvertex\Plugin\Components\Queue\Models\Task\Task;
 use Leadvertex\Plugin\Components\Queue\Models\Task\TaskAttempt;
 use Leadvertex\Plugin\Components\SpecialRequestDispatcher\Components\SpecialRequest;
@@ -38,90 +35,14 @@ class SpecialRequestTask extends Task implements ModelInterface
         return $this->request;
     }
 
-    public function send(): bool
+    public function getAttempt(): TaskAttempt
     {
-        if ($this->request->isExpired()) {
-            $this->createFailedLog(FailedRequestLog::REASON_EXPIRED);
-            $this->delete();
-            return false;
-        }
-
-        try {
-            $response = Guzzle::getInstance()->request(
-                $this->request->getMethod(),
-                $this->request->getUri(),
-                [
-                    'json' => [
-                        'request' => $this->request->getBody(),
-                        '__task' => [
-                            'createdAt' => $this->createdAt,
-                            'attempt' => [
-                                'number' => $this->attempt->getNumber(),
-                                'limit' => $this->attempt->getLimit(),
-                                'interval' => $this->attempt->getInterval(),
-                            ],
-                        ]
-                    ],
-                    'timeout' => $this->httpTimeout,
-                ]
-            );
-            $this->attempt->attempt($response->getStatusCode());
-            if ($isSuccess = $response->getStatusCode() === $this->request->getSuccessCode()) {
-                $this->delete();
-                return true;
-            }
-        } catch (BadResponseException $exception) {
-            $isSuccess = false;
-            $this->attempt->attempt($exception->getResponse()->getStatusCode());
-        } catch (Exception $exception) {
-            $isSuccess = false;
-            $this->attempt->attempt('');
-        }
-
-        if (!$isSuccess) {
-            if (in_array($this->attempt->getLog(), $this->request->getStopCodes())) {
-                $this->createFailedLog(FailedRequestLog::REASON_STOP_CODE);
-                $this->delete();
-                return false;
-            }
-
-            if ($this->attempt->isSpent()) {
-                $this->createFailedLog(FailedRequestLog::REASON_ATTEMPT);
-                $this->delete();
-                return false;
-            }
-        }
-
-        $this->save();
-        return $isSuccess;
+        return $this->attempt;
     }
 
-    public function save(): void
+    public function getHttpTimeout(): int
     {
-        $isNew = $this->isNewModel();
-        parent::save();
-
-        if ($isNew && $_ENV['LV_PLUGIN_DEBUG']) {
-            $this->send();
-        }
-    }
-
-    protected function createFailedLog(int $reason): void
-    {
-        $log = new FailedRequestLog(
-            $this->companyId,
-            $this->pluginAlias,
-            $this->pluginId,
-            $this->createdAt,
-            $this->request->getMethod(),
-            $this->request->getUri(),
-            $this->request->getBody(),
-            $this->attempt->getLastTime(),
-            $this->attempt->getNumber(),
-            $this->attempt->getLog(),
-            $reason,
-        );
-        $log->save();
+        return $this->httpTimeout;
     }
 
     protected static function beforeWrite(array $data): array
@@ -143,13 +64,14 @@ class SpecialRequestTask extends Task implements ModelInterface
     protected static function afterRead(array $data): array
     {
         $data = parent::afterRead($data);
+        $requestData = json_decode($data['request'], true);
         $request = new SpecialRequest(
-            $data['request']['method'],
-            $data['request']['uri'],
-            $data['request']['body'],
-            $data['request']['expireAt'],
-            $data['request']['successCode'],
-            $data['request']['stopCodes'] ?? [],
+            $requestData['method'],
+            $requestData['uri'],
+            $requestData['body'],
+            $requestData['expireAt'],
+            $requestData['successCode'],
+            $requestData['stopCodes'] ?? [],
         );
         $data['request'] = $request;
         return $data;
